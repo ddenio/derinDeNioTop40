5fcc9ee55cf29fddec5dff03e2cdffb2b60563e2
Derin DeNio <derindenio@gmail.com>
2025-02-17 21:58:55 -0700

    Added some styling, continued front end

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import './style.css'
 
-const client = new Client();
-client.setProject('67b40d72002657ee59f5'); 
+import { Client, Databases, ID } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('67b40d72002657ee59f5');
+
+const databases = new Databases(client);
+
+const promise = databases.createDocument(
+    '67b40e1f0009fb11ac1a',
+    '67b40e7a0011a8bb16f5',
+    ID.unique(),
+    { "title": "Hamlet" }
+);
+
+promise.then(function (response) {
+    console.log(response);
+}, function (error) {
+    console.log(error);
+});
